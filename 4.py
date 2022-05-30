4b3c7e54b61bb3891804ebf2ed90a154ff4efde7
vansh401 <106549024+vansh401@users.noreply.github.com>
2022-05-30 22:28:12 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,8 @@
+def main(): 
+    str1 ='aabcddekll12@' # first string
+    str2 ='bb2211@55k' # second string
+    count(str1, str2) # calling count function 
+  
+# Driver Code
+if __name__=="__main__":
+    main()
